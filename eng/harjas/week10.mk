1505ce5bac01a4bed560faa05174bc43c91bc138
Leo Chen <leo@harmony.one>
2022-03-03 14:54:15 -0800

    [harjas] add week10.mk as template
    Signed-off-by: Leo Chen <leo@harmony.one>

---
+++ eng/harjas/week10.mk
@@ -0,0 +1,12 @@
+# Summary
+* Owner:Harjas Singh Bathla 
+* Week 10: 03/06-03/12
+* Total: 10 hours
+* Overtime: 0 hours
+
+# Time Table
+| date  | start time  | end time | duration  |  note |
+|---|---|---|---|---|
+| 08/01  | 10:00  | 11:30  | 1.5  | sample tasks  |
+|   |   |   |   |   |
+|   |   |   |   |   |
